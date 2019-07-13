4934fe30d187ec3ce2774a72dfef3c55306ef35d
SoloEkaterina <katia-sol@mail.ru>
2019-07-13 13:28:16 +0300

    boilerplate react

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app.jsx";
+
+
+// console.log("dirname");
+// console.log( __dirname);
+// console.log("filename");
+// console.log( __filename);
+
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
